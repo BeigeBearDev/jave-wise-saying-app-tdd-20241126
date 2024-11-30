c912f03558aeab11fbda9643cae144698ab9297e
BeigeBearDev <carneo.dev@gmail.com>
2024-11-30 23:00:38 +0900

    Revert "feature: Add TestUtil.setOutToByteArray (RED)"
    This reverts commit a23b00b70e678768929ba2fde52699e7fca007fb.

--- src/test/java/com/wsa/standard/util/TestUtilTest.java
+++ src/test/java/com/wsa/standard/util/TestUtilTest.java
@@ -3,7 +3,6 @@ package com.wsa.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,19 +25,5 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("죽고자 하면 살 것이고, 살고자 하면 죽을 것이다.");
         assertThat(author).isEqualTo("이순신");
-    }
-
-    @Test
-    @DisplayName("TestUtil.setOutToByteArray()")
-    public void t2() {
-        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
-
-        System.out.println("2 / 이순신 / 죽고자 하면 살 것이고, 살고자 하면 죽을 것이다.");
-
-        String out = byteArrayOutputStream.toString().trim();
-        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
-
-        assertThat(out).isEqualTo("2 / 이순신 / 죽고자 하면 살 것이고, 살고자 하면 죽을 것이다.");
-        System.out.println("이제는 화면에 출력됩니다.");
     }
 }
