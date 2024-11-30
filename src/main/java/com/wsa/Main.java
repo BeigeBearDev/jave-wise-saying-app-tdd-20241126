bd29ca5f5f5157b288ff3ddf3535ce345af1fbc5
BeigeBearDev <carneo.dev@gmail.com>
2024-11-30 22:29:51 +0900

    feat: add lab3, 3, lab4

--- src/main/java/com/wsa/Main.java
+++ src/main/java/com/wsa/Main.java
@@ -1,13 +1,14 @@
 package com.wsa;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //lab1();
-        lab2();
+        // lab1();
+        // lab2();
+        // lab3();
+        lab4();
     }
 
     // 스캐너를 통해서 입력할 수 있는 방법
@@ -18,10 +19,9 @@ public class Main {
         String cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
-
     }
 
-    // 명령어를 미리 준비해놓고 로봇이 입력할 수 있도록 실험을 하였다.
+    // 명령어를 미리 입력시켜놓고 입력할 수 있도록 실험을 하였다.
     private static void lab2() {
         InputStream in = new ByteArrayInputStream("홍길동\n갑을병".getBytes());
         Scanner scanner = new Scanner(in);
@@ -35,6 +35,40 @@ public class Main {
         cmd  = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
 
+    // out을 통해서 입력된 사항을 print 하는 방법
+    private static void lab3() {
+        System.out.println("== lab3 시작 ==");
+
+        System.out.println("안녕하세요.");
+        System.out.println("안녕히가세요.");
+
+        System.out.println("== lab3 종료 ==");
+    }
+
+    // out을 전환시켜 출력을 제어할 수 있는 방법
+    private static void lab4() {
+        System.out.println("== lab4 시작 ==");
+
+        // 출력이 모니터로 안되도록 설정
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕하세요.");
+        System.out.println("안녕히가세요.");
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== lab4 종료 ==");
+
+        System.out.println("출력 : " + output.toString());
+
+        // output을 close(해제)하는 것은 위험성이 있어 감싸준다
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
