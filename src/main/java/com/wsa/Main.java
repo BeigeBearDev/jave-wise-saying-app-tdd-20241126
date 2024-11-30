abb282800510f6c8bf9d9c397d28080b63cb1740
BeigeBearDev <carneo.dev@gmail.com>
2024-11-30 22:15:12 +0900

    feat: modify lab1, add lab2

--- src/main/java/com/wsa/Main.java
+++ src/main/java/com/wsa/Main.java
@@ -1,19 +1,40 @@
 package com.wsa;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        //lab1();
+        lab2();
     }
 
+    // 스캐너를 통해서 입력할 수 있는 방법
     private static void lab1() {
-        Scanner cmd = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("명령 > ");
-        cmd.nextLine().trim();
+        String cmd = scanner.nextLine().trim();
 
-        System.out.println("입력한 명령 > " + cmd);
+        System.out.println("입력한 명령: " + cmd);
+
+    }
+
+    // 명령어를 미리 준비해놓고 로봇이 입력할 수 있도록 실험을 하였다.
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("홍길동\n갑을병".getBytes());
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령 > ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+        System.out.println("명령 > ");
+        cmd  = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
 
     }
 }
