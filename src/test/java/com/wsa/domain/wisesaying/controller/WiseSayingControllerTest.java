da2c16754749d1912da083234d3732e001c75f5e
BeigeBearDev <carneo.dev@gmail.com>
2024-12-02 09:05:11 +0900

    feat: Add WiseSayingController exit function (RED)

---
+++ src/test/java/com/wsa/domain/wisesaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,34 @@
+package com.wsa.domain.wisesaying.controller;
+
+import com.wsa.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("=== 명언 앱 ===")
+    public void t1() {
+        // 종료를 선입력
+        Scanner scanner = TestUtil.getScanner("종료");
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        // 앱을 실행
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        // 선입력 해제 출력
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        // 문자열 비교
+        assertThat(output)
+                .contains("=== 명언 앱 ===");
+
+    }
+}
