62c3403171dc06ffad91a95e12d2c38b05121bd0
BeigeBearDev <carneo.dev@gmail.com>
2024-12-02 15:47:40 +0900

    Test: Add test for executing commands multiple times (REFACTOR)

--- src/main/java/com/wsa/App.java
+++ src/main/java/com/wsa/App.java
@@ -1,22 +1,27 @@
 package com.wsa;
 
+import com.wsa.domain.system.controller.SystemController.SystemController;
+
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
+    private final SystemController systemController;
 
     public App(Scanner scanner){
         this.scanner = scanner;
+        this.systemController = new SystemController();
     }
 
     public void run() {
         System.out.println("=== 명언 앱 ===");
 
         while (true) {
-            System.out.println("명령 > ");
+            System.out.print("명령 > ");
             String cmd = scanner.nextLine();
 
             if ("종료".equals(cmd)) {
+                systemController.actionExit();
                 break;
             }
         }
